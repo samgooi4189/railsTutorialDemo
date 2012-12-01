418a53c59357e70deb851d0b2de5d4116a627e61
samgooi4189 <samgooi4189@samgooi4189-VirtualBox.(none)>
2012-12-01 00:46:10 -0600

    Scaffolded User and migrated db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebTutorial::Application.config.secret_token = 'd77ae83853d0e2a0821e514e3c9c8e65d51c2313dd4c799c34ec308fb6146b9bf642a0e6f75dd0956d7a8686551a98bd71df94ff4c07a6e0abefb84f80187c53'
